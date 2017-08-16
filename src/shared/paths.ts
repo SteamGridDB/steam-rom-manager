5e6ffe0ebbb226a7fb45c4c5fb9e0c17d5599309
FrogTheFrog <warliukz@gmail.com>
2017-08-16 17:30:14 +0300

    added portable configuration

--- src/shared/paths.ts
+++ src/shared/paths.ts
@@ -1,7 +1,14 @@
 import { app, remote } from 'electron';
 import * as path from 'path';
 
-export const userDataDir: string = path.join((app || remote.app).getPath('userData'), 'userData');
+let _userDataDir: string;
+
+if (process.env.PORTABLE_EXECUTABLE_DIR)
+    _userDataDir = path.join(path.dirname((app || remote.app).getPath('exe')), 'userData');
+else
+    _userDataDir = path.join((app || remote.app).getPath('userData'), 'userData');
+
+export const userDataDir: string = _userDataDir;
 export const userSettings: string = path.join(userDataDir, 'userSettings.json');
 export const userThemesDir: string = path.join(userDataDir, 'User themes');
 export const userConfigurations: string = path.join(userDataDir, 'userConfigurations.json');
