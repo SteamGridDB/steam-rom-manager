8a2ab5a698c13e2117456c8b95d0f40fcf02f913
FrogTheFrog <warliukz@gmail.com>
2017-09-26 15:45:00 +0300

    Add os variables

--- src/renderer/app.global.ts
+++ src/renderer/app.global.ts
@@ -3,5 +3,7 @@ import { languageManager } from "../shared/lib/index";
 
 export var gApp: GlobalContainer = {
     lang: languageManager.getLanguage('English'),
-    version: require('../../package.json')['version']
+    version: require('../../package.json')['version'],
+    os: require('os-name')(), 
+    arch: require('os').arch()
 };
