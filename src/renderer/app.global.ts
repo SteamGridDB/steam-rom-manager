59dbe0665fbef90c11fd68e59640a961145b4f6f
FrogTheFrog <warliukz@gmail.com>
2017-09-26 17:48:58 +0300

    update syntax

--- src/renderer/app.global.ts
+++ src/renderer/app.global.ts
@@ -1,9 +1,10 @@
 import { GlobalContainer } from "../renderer/models";
 import { languageManager } from "../shared/lib/index";
+import * as os from 'os';
 
 export var gApp: GlobalContainer = {
     lang: languageManager.getLanguage('English'),
     version: require('../../package.json')['version'],
-    os: require('os-name')(), 
-    arch: require('os').arch()
+    os: require('os-name')(os.platform(), os.release()), 
+    arch: os.arch()
 };
