cba858aac8173c4c2b5650ac5936006a93cad9dd
FrogTheFrog <warliukz@gmail.com>
2017-11-15 13:09:16 +0200

    better error class

--- src/lib/vdf-error.ts
+++ src/lib/vdf-error.ts
@@ -1,38 +1,40 @@
-const noError: string = 'No error has occurred...'
+const noError: string = 'No error has occurred...';
 
-export class VDF_Error {
-    private text: string = undefined;
+function toMessage(messages: string | string[] | VDF_Error | VDF_Error[] | Error | Error[]) {
+    let message = undefined;
 
-    constructor(messages: string | string[] | VDF_Error | VDF_Error[] | Error | Error[]) {
-        if (typeof messages === 'string')
-            this.text = messages;
-        else if (messages instanceof VDF_Error || messages instanceof Error)
-            this.text = messages.message;
-        else if (messages instanceof Array) {
-            messages = (messages as any[]).filter(error => error !== undefined && error !== null);
-            if (messages.length > 0) {
-                if (typeof messages[0] === 'string')
-                    this.text = (messages as string[]).join('\r\n');
-                else if (messages[0] instanceof VDF_Error || messages[0] instanceof Error) {
-                    this.text = (messages as any[]).map(error => error.message).join('\r\n');
-                }
+    if (typeof messages === 'string')
+        message = messages;
+    else if (messages instanceof VDF_Error || messages instanceof Error)
+        message = messages.message;
+    else if (messages instanceof Array) {
+        messages = (messages as any[]).filter(error => error !== undefined && error !== null);
+        if (messages.length > 0) {
+            if (typeof messages[0] === 'string')
+                message = (messages as string[]).join('\r\n');
+            else if (messages[0] instanceof VDF_Error || messages[0] instanceof Error) {
+                message = (messages as any[]).map(error => error.message).join('\r\n');
             }
         }
     }
 
-    get valid(){
-        return this.text !== undefined;
+    return message;
+}
+
+export class VDF_Error extends Error {
+    private isValid: boolean;
+
+    constructor(messages: string | string[] | VDF_Error | VDF_Error[] | Error | Error[]) {
+        let message = toMessage(messages);
+        super(message);
+        this.isValid = message !== undefined;
     }
 
-    get invalid(){
+    get valid() {
+        return this.isValid;
+    }
+
+    get invalid() {
         return !this.valid;
-    }
-
-    get message() {
-        return this.text || noError;
-    }
-
-    get error() {
-        return new Error(this.text);
     }
 }
