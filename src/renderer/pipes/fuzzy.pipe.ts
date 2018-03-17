203e46b8717a64ea2f1f96e133deb1680a4bd992
FrogTheFrog <warliukz@gmail.com>
2018-03-17 11:24:45 +0200

    pipe bugfix

--- src/renderer/pipes/fuzzy.pipe.ts
+++ src/renderer/pipes/fuzzy.pipe.ts
@@ -1,9 +1,9 @@
 import { Pipe, PipeTransform } from '@angular/core';
-import * as Fuzzy from 'fuzzaldrin-plus';
+const Fuzzy = require('fuzzaldrin-plus');
 
 @Pipe({ name: 'fuzzyTest' })
 export class FuzzyTestPipe implements PipeTransform {
     transform(inputString: string, query: string) {
-        return query.length > 0 ? Fuzzy.score(inputString, query) !== 0 : true;
+        return query.length > 0 ? Fuzzy.score(inputString, query, { usePathScoring: false }) !== 0 : true;
     }
 }
