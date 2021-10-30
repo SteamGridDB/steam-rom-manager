cc70dba8cdacad9d68cb8c92a44473461a49e192
Maykin <maykin.dev@protonmail.com>
2021-10-30 23:02:44 +0200

    🩹 fix image path in markdowns

--- webpack/markdown.ts
+++ webpack/markdown.ts
@@ -1,24 +1,57 @@
-import * as path from 'path';
-import { LoaderDefinition, LoaderDefinitionFunction } from 'webpack';
+import { LoaderDefinitionFunction } from 'webpack';
+
+/**
+ * The `splice()` method changes the contents of a string
+ * by removing or replacing existing elements and/or adding
+ * new elements in place.
+ *
+ * See Array version here: https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/splice
+ */
+const spliceString = (
+  input: string,
+  start: number,
+  deleteCount: number = 0,
+  ...items: string[]
+): string => {
+  return (
+    input.slice(0, start) + items.join('') + input.slice(start + deleteCount)
+  );
+};
 
 /**
  * matches strings like `[text](../some/relative/resource)`
  */
-const re = /\[(.*?)\](:\s+|\()(?!http|#)(.+?)(?:(\s.*?)(?=\))|)(\)|\s)/g;
-
-
+const re = /(\[.*?\](?::\s+|\()(?:(?!http|#)))(.+?)(?:\s.*?(?=\))|)(?:\)|\s)/g;
 
 /**
  * add in markdown referenced resources (pictures) to the webpack dependency tree
  */
-const extractResourcesInMarkdown: LoaderDefinitionFunction  = async function (source) {
-  const resolve = this.getResolve()
+const extractResourcesInMarkdown: LoaderDefinitionFunction = async function (
+  source
+) {
+  let newSource = source;
+  const resolve = this.getResolve();
   const matches = source.matchAll(re);
-  for (const match of matches) {
-    await this.importModule(await resolve(this.context, match[3]), {})
+  // if we wouldn't iterate in reverse order than the index of the 2nd+ match
+  // wouldn't match after we mutate the string
+  for (const match of Array.from(matches).reverse()) {
+    const { index, 1: preString, 2: originalPath } = match;
+    const newPath = await this.importModule(
+      await resolve(this.context, originalPath)
+    );
+    // TS suggests that `newPath` is `any` so make sure we get a string
+    if (typeof newPath !== 'string') {
+      throw new Error('Expected `importModule` to return a string');
+    }
+    newSource = spliceString(
+      newSource,
+      index + preString.length,
+      originalPath.length,
+      newPath
+    );
   }
 
-  return source
+  return newSource;
 };
 
 module.exports = extractResourcesInMarkdown;
