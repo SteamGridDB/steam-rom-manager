cf240fdbb96b9ebac389c2f4782ae46da8df5f38
Witherking25 <witherking@withertech.com>
2022-04-26 21:41:28 -0500

    also check under /var/run/host for executables

--- src/renderer/services/parsers.service.ts
+++ src/renderer/services/parsers.service.ts
@@ -311,6 +311,17 @@ export class ParsersService {
       else
         return true;
     } catch (e) {
+	  if (process.env["IN_FLATPAK"]) {
+		try {
+			let path = fs.statSync("/var/run/host" + fsPath);
+			if (checkForDirectory !== undefined)
+			  return checkForDirectory ? path.isDirectory() : path.isFile();
+			else
+			  return true;
+		} catch (e) {
+			return false;
+		}
+	  }
       return false;
     }
   }
