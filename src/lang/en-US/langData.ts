c9ff253a89b9c3a43e3c8312f582cdfc954f92d4
cbartondock <cbartondock@gmail.com>
2023-03-24 12:13:45 -0400

    moved merge into language manager, should only happen when language is changed

--- src/lang/en-US/langData.ts
+++
@@ -1,6 +0,0 @@
-import * as _ from "lodash";
-import { markdowns } from './markdowns';
-import { languageStruct } from '../../models'
-let langStrings = require("./langStrings.json")
-let langData: languageStruct =  _.merge(langStrings, markdowns) as languageStruct;
-export default langData;
