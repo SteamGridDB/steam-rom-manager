e265bed258bf3b513d22beb7f4be75e58d76c0a4
cbartondock <cbartondock@gmail.com>
2023-04-07 15:38:21 -0400

    ambient

--- ambient.d.ts
+++ ambient.d.ts
@@ -7,7 +7,6 @@ declare module 'steamgriddb';
 declare module 'steam-categories';
 declare module 'probe-image-size';
 declare module 'file-uri-to-path';
-declare module 'appid';
-declare module 'binary-vdf';
+declare module 'binary-vdf-2';
 declare module 'xdg-parse';
 declare module 'markdown-it-attrs';
