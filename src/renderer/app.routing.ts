10f4f330ed643c376e04e1e3b24f3d940c4d7edb
Rodrigo <rodrigosedano@me.com>
2023-04-11 15:15:01 +0200

    separating parsers from nav

--- src/renderer/app.routing.ts
+++ src/renderer/app.routing.ts
@@ -1,45 +1,54 @@
-import { ModuleWithProviders } from '@angular/core';
-import { Routes, RouterModule } from '@angular/router';
+import { ModuleWithProviders } from "@angular/core";
+import { Routes, RouterModule } from "@angular/router";
 
-import { AppComponent, PreviewComponent, LoggerComponent, ParsersComponent, SettingsComponent, AboutComponent, ExceptionsComponent } from './components';
-import { HrefGuard } from './guards';
+import {
+  AppComponent,
+  PreviewComponent,
+  LoggerComponent,
+  ParsersComponent,
+  SettingsComponent,
+  AboutComponent,
+  ExceptionsComponent,
+} from "./components";
+import { HrefGuard } from "./guards";
 
 const AppRouter: Routes = [
-    {
-        path: '',
-        component: PreviewComponent
-    },
-    {
-        path: 'preview',
-        component: PreviewComponent
-    },
-    {
-        path: 'logger',
-        component: LoggerComponent
-    },
-    {
-        path: 'user-exceptions',
-        component: ExceptionsComponent
-    },
-    {
-        path: 'settings',
-        component: SettingsComponent
-    },
-    {
-        path: 'parsers/:index',
-        component: ParsersComponent
-    },
-    {
-        path: 'about',
-        component: AboutComponent
-    },
-    {
-        path: '**',
-        component: AppComponent,
-        canActivate: [HrefGuard]
-    }
+  {
+    path: "",
+    component: PreviewComponent,
+  },
+  {
+    path: "preview",
+    component: PreviewComponent,
+  },
+  {
+    path: "logger",
+    component: LoggerComponent,
+  },
+  {
+    path: "user-exceptions",
+    component: ExceptionsComponent,
+  },
+  {
+    path: "settings",
+    component: SettingsComponent,
+  },
+  {
+    path: "parsers/:index",
+    component: ParsersComponent,
+  },
+  {
+    path: "about",
+    component: AboutComponent,
+  },
+  {
+    path: "**",
+    component: AppComponent,
+    canActivate: [HrefGuard],
+  },
 ];
 
 export const AppRoutes: ModuleWithProviders = RouterModule.forRoot(AppRouter, {
-    useHash: true, initialNavigation: false
+  useHash: true,
+  initialNavigation: false,
 });
--- src/renderer/components/app.component.ts
+++ src/renderer/components/app.component.ts
@@ -1,38 +1,44 @@
-import { Component, ChangeDetectionStrategy, ChangeDetectorRef, OnDestroy } from '@angular/core';
-import { SettingsService, LanguageService, MarkdownService, IpcService} from "../services";
-import { MarkdownVariable } from '../../lib';
+import { Component, ChangeDetectionStrategy, ChangeDetectorRef, OnDestroy } from "@angular/core";
+import { SettingsService, LanguageService, MarkdownService, IpcService } from "../services";
+import { MarkdownVariable } from "../../lib";
 import { Router } from "@angular/router";
-import * as highlight from 'highlight.js';
-import * as markdownIt from 'markdown-it';
+import * as highlight from "highlight.js";
+import * as markdownIt from "markdown-it";
 
 @Component({
-  selector: 'app',
+  selector: "app",
   template: `
-        <ng-container *ngIf="settingsLoaded && languageLoaded; else stillLoading">
-            <titlebar></titlebar>
-            <nav></nav>
-            <nav-border></nav-border>
-            <router-outlet style="display: none;"></router-outlet>
-            <theme></theme>
-            <alert></alert>
-            <update-notifier [ipcMessage]=ipcMessage></update-notifier>
-        </ng-container>
-        <ng-template #stillLoading>
-            <div class="appLoading"></div>
-        </ng-template>
-    `,
-  styleUrls: ['../styles/app.component.scss'],
-  changeDetection: ChangeDetectionStrategy.OnPush
+    <ng-container *ngIf="settingsLoaded && languageLoaded; else stillLoading">
+      <titlebar></titlebar>
+      <navarea></navarea>
+      <nav-border></nav-border>
+      <router-outlet style="display: none;"></router-outlet>
+      <theme></theme>
+      <alert></alert>
+      <update-notifier [ipcMessage]="ipcMessage"></update-notifier>
+    </ng-container>
+    <ng-template #stillLoading>
+      <div class="appLoading"></div>
+    </ng-template>
+  `,
+  styleUrls: ["../styles/app.component.scss"],
+  changeDetection: ChangeDetectionStrategy.OnPush,
 })
-
 export class AppComponent {
-  private ipcMessage: string = '';
+  private ipcMessage: string = "";
   private settingsLoaded: boolean = false;
   private languageLoaded: boolean = false;
-  constructor(private settingsService: SettingsService, private languageService: LanguageService, private markdownService: MarkdownService, private router: Router,private ipcService: IpcService, private changeDetectionRef: ChangeDetectorRef) {
+  constructor(
+    private settingsService: SettingsService,
+    private languageService: LanguageService,
+    private markdownService: MarkdownService,
+    private router: Router,
+    private ipcService: IpcService,
+    private changeDetectionRef: ChangeDetectorRef
+  ) {
     this.settingsService.onLoad((appSettings) => {
       this.settingsLoaded = true;
-      this.router.navigate(['/parsers', -1]);
+      this.router.navigate(["/parsers", -1]);
       this.changeDetectionRef.detectChanges();
     });
     this.languageService.observeChanges().subscribe((lang) => {
@@ -40,20 +46,26 @@ export class AppComponent {
         this.languageLoaded = true;
       }
     });
-    this.markdownService.createInstance('default', new markdownIt({
-      html: true,
-      typographer: true,
-      highlight: function (str, lang) {
-        if (lang && highlight.getLanguage(lang)) {
-          try {
-            return highlight.highlight(lang, str).value;
-          } catch (__) { }
-        }
-        return '';
-      }
-    }).use(MarkdownVariable).use(require('markdown-it-attrs')).use(require('markdown-it-anchor')));
-    ipcService.on('updater_message',(event, message)=>{
-      this.ipcMessage=message;
-    })
-  };
+    this.markdownService.createInstance(
+      "default",
+      new markdownIt({
+        html: true,
+        typographer: true,
+        highlight: function (str, lang) {
+          if (lang && highlight.getLanguage(lang)) {
+            try {
+              return highlight.highlight(lang, str).value;
+            } catch (__) {}
+          }
+          return "";
+        },
+      })
+        .use(MarkdownVariable)
+        .use(require("markdown-it-attrs"))
+        .use(require("markdown-it-anchor"))
+    );
+    ipcService.on("updater_message", (event, message) => {
+      this.ipcMessage = message;
+    });
+  }
 }
