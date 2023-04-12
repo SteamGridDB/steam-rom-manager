85ba71509abe22da211af6af3857963773acab77
cbartondock <cbartondock@gmail.com>
2023-04-12 01:21:03 -0400

    fixy fix fix

--- src/variables.ts
+++ src/variables.ts
@@ -1,16 +1,15 @@
 import { GlobalContainer } from "./models";
 import { LanguageManager } from "./lib/language-manager";
 import * as os from 'os';
-import * as fs from 'fs-extra';
 import * as process from 'process';
 import osName from 'os-name';
+import { app } from '@electron/remote'
 
-console.log()
 export const languageManager = new LanguageManager();
 export const APP: GlobalContainer = {
     lang: languageManager.getLanguage('en-US'),
     os: osName(os.platform(),os.release()),
-    version: JSON.parse(fs.readFileSync('package.json','utf-8')).version,
+    version: app.getVersion(),
     arch: os.arch(),
     srmdir: process.env.PORTABLE_EXECUTABLE_DIR||''
 };
