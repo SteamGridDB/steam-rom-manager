46da47b824fb0901614e0e1c0c14399ad5489683
cbartondock <cbartondock@gmail.com>
2023-04-12 00:32:46 -0400

    workaround weird linux issue involving require

--- src/variables.ts
+++ src/variables.ts
@@ -1,13 +1,16 @@
 import { GlobalContainer } from "./models";
 import { LanguageManager } from "./lib/language-manager";
 import * as os from 'os';
-import * as process from 'process'
+import * as fs from 'fs-extra';
+import * as process from 'process';
+import osName from 'os-name';
 
+console.log()
 export const languageManager = new LanguageManager();
 export const APP: GlobalContainer = {
     lang: languageManager.getLanguage('en-US'),
-    version: require('../package.json')['version'],
-    os: os.platform()==='win32' && parseInt(os.release().split('.').pop()) >= 22000 ? 'Windows 11' : require('os-name')(),
+    os: osName(os.platform(),os.release()),
+    version: JSON.parse(fs.readFileSync('package.json','utf-8')).version,
     arch: os.arch(),
     srmdir: process.env.PORTABLE_EXECUTABLE_DIR||''
 };
