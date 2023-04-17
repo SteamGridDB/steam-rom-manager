aa00e54c72da56eafc0e765bf6e776343247f4be
Dragoon Dorise <rodrigosedano@me.com>
2023-04-17 14:59:52 +0200

    EmuDeck Theme + Fixes on Deck and Classic navigation bar (#514)
    * Easier Theme Selector
    
    * EmuDeck wip
    
    * Deck Theme
    
    * EmuDeck theme
    
    * Opacity
    
    * small fix setttings
    
    * font
    
    * Fix routing issue (#1)
    
    * added EmuDeck to themes
    
    * fixed routing
    
    * EmuDeck Theme + fixes Classic and Deck theme
    
    ---------
    
    Co-authored-by: Chris Dock <cbartondock@gmail.com>

--- src/renderer/app.routing.ts
+++ src/renderer/app.routing.ts
@@ -1,7 +1,7 @@
 import { ModuleWithProviders } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
-import { AppComponent, PreviewComponent, LoggerComponent, ParsersComponent, SettingsComponent, AboutComponent, ExceptionsComponent } from './components';
+import { AppComponent, PreviewComponent, LoggerComponent, ParsersComponent, SettingsComponent, AboutComponent, ExceptionsComponent, ParsersListComponent } from './components';
 import { HrefGuard } from './guards';
 
 const AppRouter: Routes = [
@@ -28,6 +28,10 @@ const AppRouter: Routes = [
     {
         path: 'parsers/:index',
         component: ParsersComponent
+    },
+    {
+        path: 'parsers-list',
+        component: ParsersListComponent
     },
     {
         path: 'about',
