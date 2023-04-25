f4f52667939522a521212e35c387bb2b95664ece
cbartondock <cbartondock@gmail.com>
2023-04-25 10:11:36 -0400

    switch back to mainline better-sqlite3

--- src/main/app.ts
+++ src/main/app.ts
@@ -11,7 +11,12 @@ import { hideBin } from 'yargs/helpers';
 import { UserConfiguration } from '../models'
 
 // Crash Reporting
-// crashReporter.start({ uploadToServer: false });
+crashReporter.start({
+  productName: 'steam-rom-manager',
+  companyName: 'cbd',
+  submitURL: 'https://cbd.sp.backtrace.io:6098/post?format=minidump&token=f2caa6949cf9a39cb04a0cfef310e0479f47e6cb070b3b0cd0f4d54b97281730',
+  uploadToServer: true,
+});
 
 // CLI Setup
 let commandCLI:string = '';
