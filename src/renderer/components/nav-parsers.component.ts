b960b7d84a237f5bc5d50eb97f5372e9054bd1a1
Rodrigo <rodrigosedano@me.com>
2023-08-06 23:19:34 +0200

    New visual parsers for EmuDeck

--- src/renderer/components/nav-parsers.component.ts
+++ src/renderer/components/nav-parsers.component.ts
@@ -12,83 +12,137 @@ import { APP } from "../../variables";
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class NavParsersComponent implements OnDestroy {
-   private userConfigurations: { saved: UserConfiguration, current: UserConfiguration }[];
-   private numConfigurations: number = -1;
-   private isExceptionsUnsaved: boolean = false;
-   private dummy: boolean = true;
-   private subscriptions: Subscription = new Subscription();
-   private appSettings: AppSettings;
-   private navForm: FormGroup;
-   private navFormItems: FormArray;
+  private userConfigurations: {
+    saved: UserConfiguration;
+    current: UserConfiguration;
+    name: string;
+    restText: string;
+  }[];
+  private numConfigurations: number = -1;
+  private isExceptionsUnsaved: boolean = false;
+  private dummy: boolean = true;
+  private subscriptions: Subscription = new Subscription();
+  private appSettings: AppSettings;
+  private navForm: FormGroup;
+  private navFormItems: FormArray;
 
-   constructor(
-     private parsersService: ParsersService,
-     private languageService: LanguageService,
-     private exceptionsService: UserExceptionsService,
-     private settingsService: SettingsService,
-     private changeRef: ChangeDetectorRef,
-     private formBuilder: FormBuilder
-   ) {}
+  constructor(
+    private parsersService: ParsersService,
+    private languageService: LanguageService,
+    private exceptionsService: UserExceptionsService,
+    private settingsService: SettingsService,
+    private changeRef: ChangeDetectorRef,
+    private formBuilder: FormBuilder
+  ) {}
 
-   ngOnInit() {
-     this.appSettings = this.settingsService.getSettings();
-     this.subscriptions.add(this.parsersService.getUserConfigurations().subscribe((userConfigurations) => {
-       this.numConfigurations = userConfigurations.length;
-       this.userConfigurations = userConfigurations;
-       let someOn: boolean = userConfigurations.length ? userConfigurations.map(config=>!config.saved.disabled).reduce((x,y)=>x||y) : false;
-       this.navForm = this.formBuilder.group({
-         selectAll: someOn,
-         parserStatuses: this.formBuilder.array(userConfigurations.map((config: {saved: UserConfiguration, current: UserConfiguration}) => {
-           let singleton: {[k: string]: boolean} = {};
-           singleton[config.saved.parserId] = ! config.saved.disabled;
-           return this.formBuilder.group(singleton);
-         }))
-       });
-       this.navForm.get("selectAll").valueChanges.subscribe((val: boolean)=>{
-         if(!val || this.userConfigurations.map(config=>config.saved.disabled).reduce((x,y)=>x&&y)) {
-           this.parsersService.changeEnabledStatusAll(val);
-         }
-       });
-       this.getParserControls().forEach((control: FormControl)=>{
-         control.valueChanges.subscribe((val: {[parserId: string]: boolean}) => {
-           this.parsersService.changeEnabledStatus(Object.keys(val)[0], Object.values(val)[0])
-         })
-       })
-       this.changeRef.detectChanges();
-     }));
-     this.subscriptions.add(this.exceptionsService.isUnsavedObservable.subscribe((val:boolean)=>{
-       this.isExceptionsUnsaved = val;
-       this.refreshActiveRoute();
-       this.changeRef.detectChanges();
-     }))
-     this.languageService.observeChanges().subscribe((lang) => {
-       this.changeRef.detectChanges();
-     });
-   }
+  ngOnInit() {
+    this.appSettings = this.settingsService.getSettings();
+    this.subscriptions.add(
+      this.parsersService.getUserConfigurations().subscribe((userConfigurations) => {
+        this.numConfigurations = userConfigurations.length;
+        let alternativeValue = false;
+        let previousSeparatedValuesImg = false;
+        this.userConfigurations = userConfigurations.map(
+          (config: { saved: UserConfiguration; current: UserConfiguration }, index: number, array: any[]) => {
+            let separatedValues = config.saved.configTitle.split(" - ");
+            let separatedValuesImg = separatedValues[0].replaceAll(" ", "");
+            separatedValuesImg = separatedValuesImg.replaceAll("/", "-");
+            let imgPath = `../../src/assets/systems/${separatedValuesImg}/images/logo.svg`;
+            imgPath = imgPath.toLowerCase();
+            if (previousSeparatedValuesImg === false) {
+              previousSeparatedValuesImg = separatedValuesImg;
+            } else {
+              if (previousSeparatedValuesImg === separatedValuesImg) {
+                alternativeValue = true;
+              } else {
+                alternativeValue = false;
+              }
+            }
 
-   private flipAll() {
-     this.navForm.get('selectAll').setValue(!this.navForm.get('selectAll').value)
-   }
+            previousSeparatedValuesImg = separatedValuesImg;
+            let parserDetails = separatedValues.slice(1).join(" - ");
+            parserDetails = parserDetails.replaceAll("-", "<br/>");
+            return {
+              ...config,
+              saved: {
+                ...config.saved,
+                alternative: alternativeValue,
+                name: separatedValues[0],
+                details: parserDetails,
+                img: imgPath,
+              },
+            };
+          }
+        );
 
-   private refreshActiveRoute(){
-     this.dummy = !this.dummy;
-   }
+        console.log(this.userConfigurations);
 
-   private get lang(){
-     return APP.lang.nav.component;
-   }
+        let someOn: boolean = userConfigurations.length
+          ? userConfigurations.map((config) => !config.saved.disabled).reduce((x, y) => x || y)
+          : false;
+        this.navForm = this.formBuilder.group({
+          selectAll: someOn,
+          parserStatuses: this.formBuilder.array(
+            userConfigurations.map((config: { saved: UserConfiguration; current: UserConfiguration }) => {
+              let singleton: { [k: string]: boolean } = {};
+              singleton[config.saved.parserId] = !config.saved.disabled;
+              return this.formBuilder.group(singleton);
+            })
+          ),
+        });
 
-   getParserControls() {
-     return (this.navForm.get('parserStatuses') as FormArray).controls;
-   }
+        this.navForm.get("selectAll").valueChanges.subscribe((val: boolean) => {
+          if (!val || this.userConfigurations.map((config) => config.saved.disabled).reduce((x, y) => x && y)) {
+            this.parsersService.changeEnabledStatusAll(val);
+          }
+        });
 
-   emuClick(control: FormControl) {
-     if(this.appSettings.theme == 'EmuDeck') {
-      control.setValue(!control.value)
-     }
-   }
+        this.getParserControls().forEach((control: FormControl) => {
+          control.valueChanges.subscribe((val: { [parserId: string]: boolean }) => {
+            this.parsersService.changeEnabledStatus(Object.keys(val)[0], Object.values(val)[0]);
+          });
+        });
 
-   ngOnDestroy() {
-     this.subscriptions.unsubscribe();
-   }
- }
+        this.changeRef.detectChanges();
+      })
+    );
+
+    this.subscriptions.add(
+      this.exceptionsService.isUnsavedObservable.subscribe((val: boolean) => {
+        this.isExceptionsUnsaved = val;
+        this.refreshActiveRoute();
+        this.changeRef.detectChanges();
+      })
+    );
+
+    this.languageService.observeChanges().subscribe((lang) => {
+      this.changeRef.detectChanges();
+    });
+  }
+
+  private flipAll() {
+    this.navForm.get("selectAll").setValue(!this.navForm.get("selectAll").value);
+  }
+
+  private refreshActiveRoute() {
+    this.dummy = !this.dummy;
+  }
+
+  private get lang() {
+    return APP.lang.nav.component;
+  }
+
+  getParserControls() {
+    return (this.navForm.get("parserStatuses") as FormArray).controls;
+  }
+
+  emuClick(control: FormControl) {
+    if (this.appSettings.theme == "EmuDeck") {
+      control.setValue(!control.value);
+    }
+  }
+
+  ngOnDestroy() {
+    this.subscriptions.unsubscribe();
+  }
+}
