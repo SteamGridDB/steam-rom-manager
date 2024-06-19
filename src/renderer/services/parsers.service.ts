612ba22f07ce41da99f3ea76c8cc60e0b1ae89da
cbartondock <cbartondock@gmail.com>
2024-06-19 16:22:54 -0400

    don't preparse on validateVariable

--- src/renderer/services/parsers.service.ts
+++ src/renderer/services/parsers.service.ts
@@ -350,12 +350,9 @@ export class ParsersService {
 
               private validateVariableParserString(input: string, emptyError?: string) {
                 let canBeEmpty = !emptyError;
-                let preParser = new VariableParser({ left: '${', right: '}' });
-                let envParsed = preParser.setInput(input).parse() ? preParser.replaceVariables((variable) => {
-                  return this.fileParser.getEnvironmentVariable(variable as EnvironmentVariables,this.appSettings).trim()
-                }).trim() : input.trim();
-                if (canBeEmpty || (!canBeEmpty && envParsed.length > 0)) {
-                  return VariableParser.isValidString('${', '}', envParsed) ? null : this.lang.validationErrors.variableString__md;
+                const trimmed = input.trim();
+                if (canBeEmpty || (!canBeEmpty && trimmed.length > 0)) {
+                  return VariableParser.isValidString('${', '}', trimmed) ? null : this.lang.validationErrors.variableString__md;
                 }
                 else {
                   return emptyError;
