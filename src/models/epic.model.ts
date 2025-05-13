477c72676756d18322754372c45b85bac8e568a7
Jeffrey Farb <3586012+lexarvn@users.noreply.github.com>
2025-05-13 00:11:02 -0700

    Add in powershell script that will auto close epic when the launched game closes - Start a model for epic games manifests

---
+++ src/models/epic.model.ts
@@ -0,0 +1,8 @@
+// game json manifest
+export interface EpicGameManifest {
+  InstallLocation: string;
+  LaunchExecutable: string;
+  DisplayName: string;
+  AppName: string;
+  LaunchCommand: string;
+}
