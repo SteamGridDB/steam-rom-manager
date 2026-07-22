d6dfc41da8767fc530c75676ad789485e497635c
Marocco2 <marocco2@live.it>
2026-07-22 11:28:25 +0200

    Add `library_header_2x`

--- src/lib/artwork-types/available-artwork-types.ts
+++ src/lib/artwork-types/available-artwork-types.ts
@@ -42,7 +42,7 @@ export const artworkSingDict: Record<ArtworkType, string> = {
 
 export const steamArtworkDict: Record<ArtworkType, string[]> = {
   tall: ["library_600x900_2x.jpg", "library_600x900.jpg"],
-  long: ["header_2x.jpg", "header.jpg", "capsule_616x353.jpg", "capsule_231x87.jpg"],
+  long: ["library_header_2x.jpg", "header_2x.jpg", "header.jpg", "capsule_616x353.jpg", "capsule_231x87.jpg"],
   hero: [
     "library_hero_2x.jpg",
     "library_hero.jpg",
